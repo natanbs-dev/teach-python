d40e837cf7d74debdb10efb42941c60d8e7a8a5e
barbosa_debian <barbosa_nt@proton.me>
2025-12-19 14:16:56 -0300

    add: metodo-variaveis-essenciais

---
+++ aula_01.py
@@ -0,0 +1,50 @@
+# variaveis
+from operator import truediv
+
+texto = "hello world"
+numerico = 10
+list = ['i1', 'i2']
+dict = {"chave":"valor", "chave2":"valor2"}
+boleanos = True
+
+if False:
+    print (texto)
+    print (numerico)
+    print (dict)
+    print (list[1])
+    print (boleanos)
+print("just a comment way")
+
+#metodos
+
+""" 
+if False:
+    print("This is a comment")
+    print("This is another comment")
+print("This is not a comment")
+"""
+
+print(numerico + 10) #20
+
+def soma(a, b):
+    return a + b
+
+print(soma(numerico, 1))
+print(soma(numerico, 2))
+
+##clasess
+
+class Mamifero:
+    def andar(self):
+        print("andar")
+
+class Humano(Mamifero):
+    def falar(self):
+        print("falar")
+
+mamifero = Mamifero()
+mamifero.andar()
+
+carlos = Humano()
+carlos.andar()
+carlos.falar()
