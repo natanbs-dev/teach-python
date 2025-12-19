3995d440b43c116dc666811733c14f4088334c4b
barbosa_debian <barbosa_nt@proton.me>
2025-12-19 14:41:36 -0300

    add: inicio da rest-api

---
+++ api-rest-python.py
@@ -0,0 +1,16 @@
+# metodos basicos
+
+"""
+GET = verificar um valor
+PUT = editar um valor
+POST = criar um valor
+DELETE = deletar um valor
+PATCH = editar partes do conteudo
+"""
+
+# JSON javascript object Notation
+
+{
+    "name":"carlos",
+    "ocupation":"developer"
+}
