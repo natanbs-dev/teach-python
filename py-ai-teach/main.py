767e49d7876e86d4d4f7bdea22275b917e25f378
Natan Dev <159494554+natanbs-dev@users.noreply.github.com>
2025-12-22 12:45:47 -0300

    add: teste de ai com porcentagem

---
+++ py-ai-teach/main.py
@@ -0,0 +1,15 @@
+n1 = int(input("\nporcentagem: "))
+n2 = int(input("total : "))
+
+porc = (n1 * n2) / 100
+
+print ("====" *50 )
+
+print(f"Valor da Porcentagem: {n1}"
+      f"\nValor total: {n2}"
+      f"\nlogo, o resultado é =={porc}==  ")
+
+
+"""   
+
+"""
