f858c0ea501743010cdd47280d087dc8e32a650e
Natan Dev <159494554+natanbs-dev@users.noreply.github.com>
2025-12-29 11:09:08 -0300

    fix: listas e ciclos de manipulação

---
+++ py-ai-teach/listas.py
@@ -0,0 +1,35 @@
+# tuplas (quando usar:)
+#   quanto tiver vários itens que podem ser modificados
+
+'''
+listas = ["Beatles", "Led Zeppelin", "The Who"]
+
+# adicionar uma string a lista
+listas.append("Rolling Stones")
+
+#remover um item da lista
+listas.remove("The Who")
+
+for b in listas:
+    print("==" + b + "==")
+
+print("\n" + listas[2])
+
+# verificar quanto o tamanho da lista (len):
+print(len(listas))
+''' 
+
+# desafio de lista
+
+tarefas = []
+
+tarefas.append('1-EStudar python com IA')
+
+tarefas.append('2-EStudar java com IA')
+
+tarefas.append('3-responder emails')
+
+#print(tarefas)
+
+for tarefas in tarefas:
+    print(f'{tarefas}')
