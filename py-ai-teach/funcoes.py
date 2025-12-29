8e72336b499f8ade43642fd4017f287cecdcb87a
barbosa-linux <barbosa@proton.me.com>
2025-12-29 19:03:51 -0300

    add: funcoes recursivas

---
+++ py-ai-teach/funcoes.py
@@ -0,0 +1,16 @@
+def saudacao(nome, idade):
+    print(f"olá {nome} idade sendo {idade}" )
+
+#saudacao('carlos', 26)
+ 
+def somar(n1, n2):
+    soma= n1 + n2
+    print(soma)
+
+#somar(5,6)
+
+def calcular_desconto(preco, porcentagem):
+    return preco - (preco * porcentagem / 100)
+
+valor_final = calcular_desconto(1010, 25)
+print(f'o valor final com desconto é R${valor_final:.2f}')
